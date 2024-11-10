f13a83cb1c86956015891770a9a5b7f0eeef3158
Mandala <xiconz@qq.com>
2024-11-10 14:07:10 +0800

    fix: fix file err

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
 import {useEffect, useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [email, setEmail] = useState('test@mail.com')
+	const [password, setPassword] = useState('123456')
+	useEffect(() => {
+		fetch(`http://localhost:8000/v1/create_user`, {
+			method: 'PUT',
+			headers: {'Context-Type': 'application/json'},
+			body: JSON.stringify({email, password}),
+		}).then(res => res.json())
+			.then(data => {
+				console.log(data)
+			}).catch(err => console.error(err))
+	}, [email, password])
 
-  useEffect(() => {
-    fetch(`http://localhost:8000/v1/create_user`,{
-      method: 'PUT',
-      headers:{"Context-Type":"application/json"},
-      body: JSON.stringify({count}),
-    }).then(res => res.json())
-      .then(data=>{
-        console.log(data)
-      }).catch(err=>console.error(err))
-  },[])
+	return (
+		<>
+			<label htmlFor="email"></label>
+			<input id='email' type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
 
-  return (
-    <>
-
-    </>
-  )
+			<label htmlFor="password"></label>
+			<input id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+		</>
+	)
 }
 
 export default App
