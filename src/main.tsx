c63cd8ceb426e73daf76f591ca532967ea6e6f0d
Sumery <sumnery@qq.com>
2025-04-19 15:20:21 +0800

    perf: use Joy UI and Material UI together in the same project

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,17 @@ import {createRouter, RouterProvider} from '@tanstack/react-router'
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
 import {routeTree} from './routeTree.gen'
-import {CssVarsProvider} from '@mui/joy/styles'
+import {CssVarsProvider, CssVarsProvider as JoyCssVarsProvider} from '@mui/joy/styles'
 import theme from '@/core/conf/theme'
 import './app.css'
 import {LanguageProvider} from '@/core/providers/I18nProvider/LanguageContext'
 import {I18nProvider} from '@/core/providers/I18nProvider'
 import {scan} from "react-scan";
 import AlertProvider from "@/core/providers/AlertProvider";
+import {createTheme, THEME_ID as MATERIAL_THEME_ID, ThemeProvider,} from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const materialTheme = createTheme();
 
 // 创建路由
 const router = createRouter({
@@ -42,7 +46,12 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
             <AlertProvider>
                 <I18nProvider>
                     <LanguageProvider>
-                        <RouterProvider router={router}/>
+                        <ThemeProvider theme={{[MATERIAL_THEME_ID]: materialTheme}}>
+                            <JoyCssVarsProvider>
+                                <CssBaseline enableColorScheme/>
+                                <RouterProvider router={router}/>
+                            </JoyCssVarsProvider>
+                        </ThemeProvider>
                     </LanguageProvider>
                 </I18nProvider>
             </AlertProvider>
